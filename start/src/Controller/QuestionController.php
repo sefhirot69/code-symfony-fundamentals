deaaf5e7e3564c440569636d1bb9def5072f5a1a
sefhirot69 <josalillo@gmail.com>
2021-10-05 09:25:45 +0200

    pruebo guardar texto en cacheInterface

--- start/src/Controller/QuestionController.php
+++ start/src/Controller/QuestionController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -27,8 +29,9 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
+     * @throws InvalidArgumentException
      */
-    public function show($slug, MarkdownParserInterface $markdownParser): Response
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache): Response
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -36,12 +39,17 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
 
-        $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
-//        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+        $questionText = "I've been turned into a cat, any **thoughts** on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function () use (
+            $markdownParser,
+            $questionText) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $questionText,
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }

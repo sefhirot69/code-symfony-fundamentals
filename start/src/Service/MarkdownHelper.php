6d45c2e5d62bd080f1bb20984d3f5d5a62bd5e74
sefhirot69 <josalillo@gmail.com>
2021-10-06 10:48:39 +0200

    feat : named autowiring

--- start/src/Service/MarkdownHelper.php
+++ start/src/Service/MarkdownHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 final class MarkdownHelper
@@ -22,26 +23,41 @@ final class MarkdownHelper
      * @var bool
      */
     private $isDebug;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(CacheInterface $cache, MarkdownParserInterface $markdownParser, bool $isDebug)
-    {
+    public function __construct(
+        CacheInterface $cache,
+        MarkdownParserInterface $markdownParser,
+        bool $isDebug,
+        LoggerInterface $markdownLogger
+    ) {
 
         $this->cache = $cache;
         $this->markdownParser = $markdownParser;
         $this->isDebug = $isDebug;
+        $this->logger = $markdownLogger;
     }
 
-    public function parse(string $source) : string {
+    public function parse(string $source): string
+    {
 
-        if($this->isDebug) {
+        if(stripos($source, 'cat') !== false){
+            $this->logger->info('Meow!');
+        }
+
+        if ($this->isDebug) {
             return $this->markdownParser->transformMarkdown($source);
         }
 
-        return  $this->cache->get('markdown_' . md5($source), function () use (
+        return $this->cache->get('markdown_' . md5($source), function () use (
             $source
         ) {
 
             return $this->markdownParser->transformMarkdown($source);
         });
     }
+
 }

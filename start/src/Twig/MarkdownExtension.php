84756ead81becda5e662cd3be8551d1beb2caa90
sefhirot69 <josalillo@gmail.com>
2021-10-08 09:38:40 +0200

    feat : Twig extension

---
+++ start/src/Twig/MarkdownExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class MarkdownExtension extends AbstractExtension
+{
+
+    /**
+     * @var MarkdownHelper
+     */
+    private $markdownHelper;
+
+    public function __construct(MarkdownHelper $markdownHelper)
+    {
+
+        $this->markdownHelper = $markdownHelper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('parse_markdown', [$this, 'parseMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function parseMarkdown(string $value): string
+    {
+        return $this->markdownHelper->parse($value);
+    }
+}
